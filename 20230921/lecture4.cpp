1e10daad57550396d387c55707d42319a88faf26
NayulLee <144967424+NayulLee@users.noreply.github.com>
2023-09-21 13:00:43 +0900

    Update lecture4.cpp

--- 20230921/lecture4.cpp
+++ 20230921/lecture4.cpp
@@ -1,5 +1,5 @@
 // 반복문/조건문 실습 : 몬테 - 카를로 알고리즘
-#define MAX_COUNT 1000
+#define MAX_COUNT 1000000000 // 시행 횟수 10억회
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -21,7 +21,6 @@
             count++;
             
             if ((x * x) + (y * y) <= 1) {
-
                 circle++;
             }
         }
@@ -29,7 +28,7 @@
     }
 } */
 
-int main() {
+/* int main() {
 
     int i;
     double x = 0;
@@ -51,4 +50,25 @@ int main() {
         }
         printf("%d진행.. 원주율 : %0.15f\n", (progress / MAX_COUNT) * 100, (InCircle / progress) * 4);
     }
+} */
+
+int main() {
+
+    double i = 0;
+    int count;
+    double InCircle = 0;
+    double x, y;
+
+    for (count = 0; count < MAX_COUNT; count++) {
+        x = (double)rand() / (double)RAND_MAX;
+        y = (double)rand() / (double)RAND_MAX;
+        i++;
+        if ((x * x) + (y * y) <= 1) { // 점의 좌표가 원 내부에 존재할 경우
+            InCircle++;
+        }
+
+        printf("%.2f%%진행.. 원주율 : %0.15f\n", (i / MAX_COUNT) * 100, (InCircle / count) * 4);
+    }
+
+    return 0;
 }
